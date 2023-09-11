d5c40193c9d9b7bd4ec6baa2d6a4e5134be83a61
MillieQiu <millie.yi110@gmail.com>
2023-09-12 00:50:51 +0800

    First upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 相關 html 標籤語法放置的地方 -->
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+// 程式邏輯放置的地方
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+/* 相關 css 標籤語法放置的地方 */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue' //在 src/main.js 內引用 App.vue 
+
+createApp(App).mount('#app') // 渲染到 public/index.html
